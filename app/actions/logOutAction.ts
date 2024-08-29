0afba42b9811758785985808fe737d21438d307b
Adil Ashurov <ashurovadil96@gmail.com>
2024-08-29 07:22:33 +0400

    set logout cookie

--- app/actions/logOutAction.ts
+++ app/actions/logOutAction.ts
@@ -1,8 +1,13 @@
 'use server'
 import { cookies } from "next/headers";
 
-
+const config = {
+    path: "/",
+    domain: process.env.HOST ?? "localhost",
+    maxAge: -1
+  };
+  
 
 export async function logOutAction() {
-    cookies().delete("jwt");
+    cookies().set("jwt", '', config );
 }
